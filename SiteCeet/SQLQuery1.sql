eba1efa04d510fb43b3db29ba8c39a5f82254fa0
lorena brito <lorenabrito051@outlook.com>
2021-03-12 14:38:56 -0300

    add

---
+++ SiteCeet/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿create table usuario (
+    userID int not null primary key IDENTITY(1,1),
+    userNome nvarchar(150) NOT NULL,
+    userEmail nvarchar(150) NOT NULL,
+    userTel nvarchar(20) 
+);
+
